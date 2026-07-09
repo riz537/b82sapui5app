427dd258ee8152b8e4a11a7fed8bacd793ab376f
Rizwan Trainings <rizwantrainingsb82p1@gmail.com>
2026-07-09 03:46:49 +0000

    validations

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,35 +1,57 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+], (Controller, MessageBox) => {
     "use strict";
 
     return Controller.extend("com.demo.b82sapui5app.controller.View1", {
         onInit() {
         },
-        onPress:function(){
+        onPress: function () {
             this.getOwnerComponent().getRouter().navTo("RouteView2");
         },
-        onSubmit:function(){
-            var name = this.getView().byId("oIpName").getValue();
-            var newWelcomeText = name +", Welcome to SAP UI5";
-            this.getView().byId("oTxtWelcome").setText(newWelcomeText); 
-            var alignment = this.getView().byId("oIpAlignment").getValue();
-            this.getView().byId("oTxtWelcome").setTextAlign(alignment);
-            var signal = this.getView().byId("oIpSignal").getValue();
+        onSubmit: function () {
+            var allIsWell = true;
+            var empId = this.byId("oIpEmpId").getValue();
+            var email = this.byId("oIpEmail").getValue();
 
-            this.getView().byId("oBtnRed").setType("Default");
-            this.getView().byId("oBtnBlue").setType("Default");
-            this.getView().byId("oBtnGreen").setType("Default");
-            
-            if(signal === "red"){
-                this.getView().byId("oBtnRed").setType("Reject");
-            } 
-            else if(signal === "blue"){
-                this.getView().byId("oBtnBlue").setType("Emphasized");
+            if (empId === "") {
+                allIsWell = false;
+                this.byId("oIpEmpId").setValueState("Error");
+                this.byId("oIpEmpId").setValueStateText("Employee ID is required");
+            } else {
+                this.byId("oIpEmpId").setValueState("None");
+                // here u need to wrtie data format validation code for empId
+                var empIdRegExp = /^\d{6}$/;
+                if (!empId.match(empIdRegExp)) {
+                    allIsWell = false;
+                    this.byId("oIpEmpId").setValueState("Error");
+                    this.byId("oIpEmpId").setValueStateText("Invalid Employee ID format");
+                }
             }
-            else if(signal === "green"){
-                this.getView().byId("oBtnGreen").setType("Accept");
-            }   
+            if (email === "") {
+                allIsWell = false;
+                this.byId("oIpEmail").setValueState("Error");
+                this.byId("oIpEmail").setValueStateText("Email ID is required");
+            } else {
+                this.byId("oIpEmail").setValueState("None");
+                // here u need to wrtie data format validation code for email
+                var emailRegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+                if (!email.match(emailRegExp)) {
+                    allIsWell = false;
+                    this.byId("oIpEmail").setValueState("Error");
+                    this.byId("oIpEmail").setValueStateText("Invalid Email ID format");
+                }
+            }
+
+            // send this data to backend using oData service 
+            if (allIsWell === true) {
+                MessageBox.success("Data submitted successfully");
+
+                // code to send data to backend using oData service
+            }
+
+
         }
     });
 });
