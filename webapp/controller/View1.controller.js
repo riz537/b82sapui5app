1ed29d8c69a9bc075301d3d2fb0aabebda9ebd4b
Rizwan Trainings <rizwantrainingsb82p1@gmail.com>
2026-07-17 03:41:41 +0000

    multi value UI elements

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,57 +1,61 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
-], (Controller, MessageBox) => {
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageBox, JSONModel) => {
     "use strict";
 
     return Controller.extend("com.demo.b82sapui5app.controller.View1", {
         onInit() {
+            var oModel = this.getOwnerComponent().getModel("oModel");
+            var empModel = new JSONModel();
+            this.getView().setModel(empModel, "empModel");
+
+            //var empModel = this.getOwnerComponent().getModel("empModel");
+            oModel.read("/EmployeeSet", {
+                success: function (data) {
+                    // if i have to process the data before setting it to empModel, i can do it here
+                    for (var i = 0; i < data.results.length; i++) {
+                        var jobLevel = "";
+                        if (data.results[i].Desig === "DEVELOPER") {
+                            jobLevel = "JL1";
+                        } else if (data.results[i].Desig === "SENIOR DEVELOPER") {
+                            jobLevel = "JL2";
+                        } else if (data.results[i].Desig === "TEAM LEAD") {
+                            jobLevel = "JL3";
+                        } else if (data.results[i].Desig === "MANAGER") {
+                            jobLevel = "JL4";
+                        }
+                        data.results[i].Desig = data.results[i].Desig + " (" + jobLevel + ")";
+                    }
+                    empModel.setData(data);
+                }.bind(this)
+            });
         },
         onPress: function () {
             this.getOwnerComponent().getRouter().navTo("RouteView2");
         },
         onSubmit: function () {
-            var allIsWell = true;
-            var empId = this.byId("oIpEmpId").getValue();
-            var email = this.byId("oIpEmail").getValue();
-
-            if (empId === "") {
-                allIsWell = false;
-                this.byId("oIpEmpId").setValueState("Error");
-                this.byId("oIpEmpId").setValueStateText("Employee ID is required");
-            } else {
-                this.byId("oIpEmpId").setValueState("None");
-                // here u need to wrtie data format validation code for empId
-                var empIdRegExp = /^\d{6}$/;
-                if (!empId.match(empIdRegExp)) {
-                    allIsWell = false;
-                    this.byId("oIpEmpId").setValueState("Error");
-                    this.byId("oIpEmpId").setValueStateText("Invalid Employee ID format");
-                }
-            }
-            if (email === "") {
-                allIsWell = false;
-                this.byId("oIpEmail").setValueState("Error");
-                this.byId("oIpEmail").setValueStateText("Email ID is required");
-            } else {
-                this.byId("oIpEmail").setValueState("None");
-                // here u need to wrtie data format validation code for email
-                var emailRegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-                if (!email.match(emailRegExp)) {
-                    allIsWell = false;
-                    this.byId("oIpEmail").setValueState("Error");
-                    this.byId("oIpEmail").setValueStateText("Invalid Email ID format");
-                }
-            }
-
-            // send this data to backend using oData service 
-            if (allIsWell === true) {
-                MessageBox.success("Data submitted successfully");
-
-                // code to send data to backend using oData service
-            }
-
-
+            // code to read data from multi value UI elements
+            var selValue = this.getView().byId("oSel").getSelectedKey();
+            var cbValue = this.getView().byId("oCB").getSelectedKey();
+            var mcbValue = this.getView().byId("oMCB").getSelectedKeys();
+            var rbgValue = this.getView().byId("oRBG").getSelectedIndex();    
+        },
+        onSelectChange:function(){
+             var selValue = this.getView().byId("oSel").getSelectedKey();
+             if(selValue === "JAGADEESH"){
+               this.getView().byId("oCB").setEnabled(false);
+             }
+        },
+        onSelFromCB:function(){
+             var cbValue = this.getView().byId("oCB").getSelectedKey();
+        },
+        onSelFromMCB:function(){
+             var mcbValue = this.getView().byId("oMCB").getSelectedKeys();
+        },
+        onSelectFromRBG:function(){
+             var rbgValue = this.getView().byId("oRBG").getSelectedIndex();
         }
     });
 });
